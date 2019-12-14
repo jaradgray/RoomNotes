183fefb31ce094d0575ffbb25f4e07070ecbb811
Jarad Gray <jarad.h.gray@gmail.com>
2019-12-14 10:29:36 -0600

    set up RecyclerView in MainActivity

--- app/src/main/java/com/jgendeavors/roomnotes/MainActivity.java
+++ app/src/main/java/com/jgendeavors/roomnotes/MainActivity.java
@@ -3,9 +3,12 @@ package com.jgendeavors.roomnotes;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.jgendeavors.roomnotes.adapters.NoteAdapter;
 import com.jgendeavors.roomnotes.entities.Note;
 import com.jgendeavors.roomnotes.viewmodels.NoteViewModel;
 
@@ -21,15 +24,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Initialize RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.activity_main_recyclerview);
+        // Every RecyclerView needs a LayoutManager. Our RecyclerView will display items
+        // in a vertical list, so we need a LinearLayoutManager
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true); // improves performance when we know the size of our RecyclerView in the layout won't change
+        // Every RecyclerView needs a RecyclerView.Adapter. We'll need a reference to it, too.
+        final NoteAdapter adapter = new NoteAdapter();
+        recyclerView.setAdapter(adapter);
+
         // Request a NoteViewModel from the Android system
         mNoteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
 
         // observe the ViewModel's LiveData
         mNoteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            /**
+             * Called each time the data in the LiveData object we're observing changes.
+             * @param notes
+             */
             @Override
             public void onChanged(List<Note> notes) {
-                // called each time the data in the LiveData object changes
-                // TODO update UI
+                // update RecyclerView UI
+                adapter.setNotes(notes);
             }
         });
     }
