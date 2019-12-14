2f9e0f06ec2e9ae7bb55ee67da8c2025908b1d07
Jarad Gray <jarad.h.gray@gmail.com>
2019-12-13 18:32:55 -0600

    add NoteViewModel class

---
+++ app/src/main/java/com/jgendeavors/roomnotes/viewmodels/NoteViewModel.java
@@ -0,0 +1,13 @@
+package com.jgendeavors.roomnotes.viewmodels;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class NoteViewModel extends AndroidViewModel {
+
+    public NoteViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
