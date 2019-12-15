8d9f7d57ef53db7ceceed2db44d6023e7b291016
Jarad Gray <jarad.h.gray@gmail.com>
2019-12-15 13:36:52 -0600

    implement basics of NoteActivityViewModel

--- app/src/main/java/com/jgendeavors/roomnotes/viewmodels/NoteActivityViewModel.java
+++ app/src/main/java/com/jgendeavors/roomnotes/viewmodels/NoteActivityViewModel.java
@@ -1,17 +1,29 @@
 package com.jgendeavors.roomnotes.viewmodels;
 
-import com.jgendeavors.roomnotes.entities.Note;
+import android.app.Application;
 
+import com.jgendeavors.roomnotes.entities.Note;
+import com.jgendeavors.roomnotes.repositories.NoteRepository;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.ViewModel;
 
 /**
  * The NoteActivityViewModel stores and processes data that affects the UI of NoteActivity,
  * and communicates user interaction to the model (Repository)
  */
-public class NoteActivityViewModel extends ViewModel {
+public class NoteActivityViewModel extends AndroidViewModel {
 
+    private NoteRepository mRepository;
+
+    public NoteActivityViewModel(@NonNull Application application) {
+        super(application);
+
+        mRepository = new NoteRepository(application);
+    }
 
     // API Methods this ViewModel exposes
-    public void insert(Note note) {}
-    public void update(Note note) {}
+    public void insert(Note note) { mRepository.insert(note); }
+    public void update(Note note) { mRepository.update(note); }
 }
